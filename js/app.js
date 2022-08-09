839093c9b4e1227aed68e18f65594908a31a94f8
Antoine <ap@gmail.com>
2022-08-09 18:03:52 +0200

    "modifications + newsletter"

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let email = document.getElementById('emailInput'),
+    divMessage = document.getElementById('divMessage'),
+    register = document.getElementById('register');
+
+function register(email){
+    
+}
